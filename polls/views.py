c4f51ec5a304ce3982e96a51948e810033ab0ea2
Andrew <diver.vlz@gmail.com>
2022-06-13 13:05:50 +0300

    start project files

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, HttpResponse
 
 def main(request):
     context = {
-        'title': 'Mediaportal'
+        'title': 'Mediaportal',
+        'greeting': 'Welcome %user_name!'
     }
-    return render(request, 'base.html', context)
+    return render(request, 'index.html', context)
