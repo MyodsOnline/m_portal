6adefdbb75d100677777a8da55b99f84d011a805
Andrew Smith <diver.vlz@gmail.com>
2022-06-15 16:15:19 +0300

    Update header & link to docs

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,5 @@ from .views import main, ContentView, SingleContentView
 
 urlpatterns = [
     path('', ContentView.as_view(), name='polls'),
-    path('<str:slug>/', SingleContentView.as_view(), name='single'),
+    path('name/<str:slug>/', SingleContentView.as_view(), name='single'),
 ]
