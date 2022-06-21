d85a5420b2c849ff4448e90ec20653360258d1c1
Andrew Smith <diver.vlz@gmail.com>
2022-06-21 21:27:58 +0300

    added ability to search by title

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 
-from .views import main, ContentView, SingleContentView, CreateContentView, AddComment, TagContentView, CategoryContentView
+from .views import main, ContentView, SingleContentView, CreateContentView, AddComment, \
+    TagContentView, CategoryContentView, Search
 
 urlpatterns = [
     path('', ContentView.as_view(), name='polls'),
+    path('search/', Search.as_view(), name='search'),
     path('add/', CreateContentView.as_view(), name='add'),
     path('<str:slug>/', SingleContentView.as_view(), name='single'),
     path('category/<str:slug>', CategoryContentView.as_view(), name='category'),
